0447124b09e1624b3c3aee15adf7139742a328a3
LutyiSan <lucenkoaktavest@gmail.com>
2022-01-27 17:23:56 +0300

    add web-server Starlett  27.01.22

--- web-server/app.py
+++ web-server/app.py
@@ -16,5 +16,5 @@ routes = [
 ]
 
 app = Starlette(debug=True, routes=routes)
-app = Starlette(debug=True, routes=routes)
-uvicorn.run(app, host="0.0.0.0", port=8000)
+
+uvicorn.run(app, host="127.0.0.1", port=8000)
--- web-server/static/js/main.js
+++ web-server/static/js/main.js
@@ -2,10 +2,17 @@
 window.onload = function () {
 
     //получаем идентификатор элемента
-    var a = document.querySelector('#id1');
+    var a = document.querySelector('.create');
+    var b = document.querySelector('#center');
     //вешаем на него событие
-    a.onclick = function () {
+    a.onmouseover = function () {
         //производим какие-то действия
-        a.innerHTML = "<h2>Привет, javascript!</h2><br><h3>Это круто я работаю<h3>";
+        console.log(a)
+        b.innerHTML = "<h2>Это уже JAVA-SCRIPT</h2>";
+        }
+       a.onmouseout = function () {
+        //производим какие-то действия
+        console.log(a)
+        b.innerHTML = "<h2>PYTHON+JAVA-SCRIPT захватит МИР!)</h2>";
     }
 }
