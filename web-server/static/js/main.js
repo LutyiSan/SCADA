0d2e93ee84146d27c4be7e729cfa12c31e7ec073
LutyiSan <lucenkoaktavest@gmail.com>
2022-01-28 18:55:44 +0300

    qqqq

--- web-server/static/js/main.js
+++ web-server/static/js/main.js
@@ -12,11 +12,19 @@ formElem.style.display = 'none';
 });
 };
 
+function formDisplayClose(close, form){
+let closeElem  = document.querySelector(close);
+let formElem = document.querySelector(form);
+closeElem.addEventListener("click", function(){
+formElem.style.display = 'none';
+});
+};
+
 function typeTag(element, click){
 let elem = document.querySelector(element);
 let cl = document.querySelector(click);
 cl.addEventListener('click', function(){
-    if (elem.name == 'modbus'){
+    if (elem.value === 'modbus'){
         document.querySelector('#modbus-tag').style.display = 'block';
         document.querySelector('#type-tag').style.display = 'none';
         }
@@ -34,5 +42,7 @@ formDisplay("#new-project", "#close-save-project","#save-project");
 formDisplay("#new-device", "#close-save-device","#save-device");
 formDisplay("#new-tags", "#close-type-tag","#type-tag");
 typeTag('#sel-tag', '#save-type-tag')
-formDisplay("#tags", "#close-type-tag","#type-tag");
+formDisplayClose("#close-type-tag","#type-tag");
+formDisplayClose("#close-bacnet-tag","#bacnet-tag");
+formDisplayClose("#close-modbus-tag","#modbus-tag");
 }
