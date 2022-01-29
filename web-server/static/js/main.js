61a81f40111d429686f8ee3a383791cc442f2bdb
LutyiSan <lucenkoaktavest@gmail.com>
2022-01-29 12:13:20 +0300

    qqqq

--- web-server/static/js/main.js
+++ web-server/static/js/main.js
@@ -1,10 +1,12 @@
 
 
-function formDisplay(open,close, form){
+
+function elemDisplay(open,close, form){
 let openElem = document.querySelector(open);
 let closeElem  = document.querySelector(close);
 let formElem = document.querySelector(form);
 openElem.addEventListener("click", function(){
+
 formElem.style.display = 'block';
 });
 closeElem.addEventListener("click", function(){
@@ -12,13 +14,33 @@ formElem.style.display = 'none';
 });
 };
 
-function formDisplayClose(close, form){
+function elemClose(close, form){
 let closeElem  = document.querySelector(close);
 let formElem = document.querySelector(form);
 closeElem.addEventListener("click", function(){
 formElem.style.display = 'none';
 });
 };
+
+function elemOpen(close, form){
+let closeElem  = document.querySelector(close);
+let formElem = document.querySelector(form);
+closeElem.addEventListener("click", function(){
+formElem.style.display = 'block';
+});
+};
+
+function resetForm(command,form){
+let elem = document.querySelector(form);
+let action = document.querySelector(command);
+console.log(elem);
+console.log(action);
+action.addEventListener("click", function(){
+elem.reset();
+});
+};
+
+
 
 function typeTag(element, click){
 let elem = document.querySelector(element);
@@ -28,7 +50,7 @@ cl.addEventListener('click', function(){
         document.querySelector('#modbus-tag').style.display = 'block';
         document.querySelector('#type-tag').style.display = 'none';
         }
-    else{
+    else if (elem.value === 'bacnet'){
         document.querySelector('#bacnet-tag').style.display = 'block';
         document.querySelector('#type-tag').style.display = 'none';
         }
@@ -38,11 +60,25 @@ cl.addEventListener('click', function(){
 
 
 window.onload = function () {
-formDisplay("#new-project", "#close-save-project","#save-project");
-formDisplay("#new-device", "#close-save-device","#save-device");
-formDisplay("#new-tags", "#close-type-tag","#type-tag");
+elemDisplay("#new-project", "#close-save-project","#save-project");
+elemDisplay("#new-device", "#close-save-device","#save-device");
+elemDisplay("#new-tags", "#close-type-tag","#type-tag");
 typeTag('#sel-tag', '#save-type-tag')
-formDisplayClose("#close-type-tag","#type-tag");
-formDisplayClose("#close-bacnet-tag","#bacnet-tag");
-formDisplayClose("#close-modbus-tag","#modbus-tag");
+elemClose("#close-type-tag","#type-tag");
+elemClose("#close-bacnet-tag","#bacnet-tag");
+elemClose("#close-modbus-tag","#modbus-tag");
+resetForm("#next-device", "#save-device");
+resetForm("#next-bacnet-tag", "#bacnet-tag");
+resetForm("#next-modbus-tag", "#modbus-tag");
+
+
+
+
+
+
+
+
+
+
+
 }
